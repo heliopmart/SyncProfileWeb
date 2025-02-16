0efd1195b2bbb05b54ca73a0228abb1b3d10e4a4
Hélio Martins <helioperesmartinsneto@gmail.com>
2025-02-16 13:48:20 -0300

    create SEOs files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    siteUrl: "https://profile-web-git-main-heliopmarts-projects.vercel.app/",
-    generateRobotsTxt: true,
-};
+    siteUrl: "https://profile-web-git-main-heliopmarts-projects.vercel.app", // Substitua pelo seu domínio real se for diferente
+    generateRobotsTxt: true, // Gera também um arquivo robots.txt
+    sitemapSize: 5000, // Garante que o sitemap inclua todas as páginas
+    exclude: ["/404"], // Exclui páginas desnecessárias, como erro 404
+    changefreq: "daily", // Define a frequência de atualização
+    priority: 0.7,
+  };
